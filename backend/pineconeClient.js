37790f5eaad8c8b39d0cd762b615c7ed23ba6053
Himaghna <himuraja2006@gmail.com>
2026-05-23 18:03:28 +0530

    pinecone connection and api keys

---
+++ backend/pineconeClient.js
@@ -0,0 +1,42 @@
+import { Pinecone } from '@pinecone-database/pinecone'
+
+const { PINECONE_API_KEY, PINECONE_INDEX_NAME } = process.env
+
+if (!PINECONE_API_KEY) {
+  throw new Error("Missing PINECONE_API_KEY in environment variables")
+}
+
+if (!PINECONE_INDEX_NAME) {
+  throw new Error("Missing PINECONE_INDEX_NAME in environment variables")
+}
+
+const pinecone = new Pinecone({
+  apiKey: PINECONE_API_KEY,
+})
+
+const index = pinecone.index({ 
+  name: PINECONE_INDEX_NAME
+ })
+
+
+const verifyConnection = async () => {
+  try {
+    const indexDescription = await pinecone.describeIndex(process.env.PINECONE_INDEX_NAME)
+    console.log("Pinecone connected successfully ")
+    return indexDescription
+  } catch (error) {
+    console.error("Pinecone connection failed ", error.message)
+    throw new Error(`Pinecone connection failed: ${error.message}`)
+  }
+}
+
+const clearIndex = async () => {
+  try {
+    await index.deleteAll()
+    console.log("Pinecone index cleared ")
+  } catch (error) {
+    console.error("Failed to clear index ", error.message)
+  }
+}
+
+export { index ,verifyConnection,clearIndex}
