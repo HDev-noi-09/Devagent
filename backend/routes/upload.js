99e6cb87fd0106175f0c7a261e9075abda9ca41a
Himaghna <himuraja2006@gmail.com>
2026-06-22 17:09:27 +0530

    redeploying

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,13 +1,20 @@
 import express from 'express'
 import multer from 'multer'
 import path from 'path'
+import fs from "fs";
 import { uploadController } from '../controllers/uploadController.js'
 
 const router = express.Router()
 
+
+const uploadDir = "uploads";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, uploadDir)
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}_${file.originalname}`)
